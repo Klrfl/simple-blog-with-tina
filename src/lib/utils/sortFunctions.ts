ed4282fa3a4f5b8f26e87471a63296ad3a4c4cea
Efraim Munthe <efrayanglain@gmail.com>
2024-08-14 11:38:27 +0700

    add additional prettier rules

--- src/lib/utils/sortFunctions.ts
+++ src/lib/utils/sortFunctions.ts
@@ -3,7 +3,7 @@ export const sortByDate = (array: any[]) => {
 	const sortedArray = array.sort(
 		(a: any, b: any) =>
 			new Date(b.data.date && b.data.date) -
-			new Date(a.data.date && a.data.date),
+			new Date(a.data.date && a.data.date)
 	);
 	return sortedArray;
 };
